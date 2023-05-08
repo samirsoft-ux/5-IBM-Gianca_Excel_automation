3c66df233b7d443c0bee489e57076403650c6ad4
Italo Silva <Italo.Silva.Silvera@outlook.com>
2023-05-08 17:10:01 -0500

    First commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install opencv-python
+pip install -U pip
+pip install -U matplotlib
+pip install numpy
